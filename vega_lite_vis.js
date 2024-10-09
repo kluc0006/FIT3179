1fe9fed75d1e1dce5f598ffd966b2a9b66b2a283
kluc0006 <126427735+kluc0006@users.noreply.github.com>
2024-10-09 22:11:08 +1100

    inital draft of data visualisation 2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,24 @@
-var vg_1 = "historical_bushfire_trend.vg.json";
-vegaEmbed("#vis", vg_1).then(function(result) {
+var vg_1 = "./fire_source_early.vg.json";
+vegaEmbed("#pie1", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+    
+var vg_2 = "./fire_source_late.vg.json";
+vegaEmbed("#pie2", vg_2).then(function (result) {
+
+}).catch(console.error);
+
+var vg_3 = "./historical_bushfire_trend_temp.vg.json";
+vegaEmbed("#linebar", vg_3, {"actions": false}).then(function (result){
+
+}).catch(console.error);
+
+var vg_4 = "./bushfire_map.vg.json";
+vegaEmbed("#firemap", vg_4).then(function (result) {
+
+}).catch(console.error);
+
+var vg_5 = "./temperature_map.vg.json";
+vegaEmbed("#tempmap", vg_5).then(function (result) {
+
 }).catch(console.error);
