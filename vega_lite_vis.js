e6c372513ed92f061622c89aeee328e48eaff8d3
kluc0006 <126427735+kluc0006@users.noreply.github.com>
2024-10-15 14:56:49 +1100

    data vis-2 updated index page

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,15 +1,10 @@
-var vg_1 = "./fire_source_early.vg.json";
-vegaEmbed("#pie1", vg_1).then(function (result) {
+var vg_1 = "./butterfly_chart.vg.json";
+vegaEmbed("#pie1", vg_1, {"actions": false }).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-    
-var vg_2 = "./fire_source_late.vg.json";
-vegaEmbed("#pie2", vg_2).then(function (result) {
-
 }).catch(console.error);
 
 var vg_3 = "./historical_bushfire_trend_temp.vg.json";
-vegaEmbed("#linebar", vg_3, {"actions": false}).then(function (result){
+vegaEmbed("#linebar", vg_3).then(function (result){
 
 }).catch(console.error);
 
